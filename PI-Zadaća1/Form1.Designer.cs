fd6f333eec9550c38d3ca559358239e4e60fa5cd
Borna Šoštar <bsostar21@foi.hr>
2023-05-01 13:21:57 +0200

    Create information display #1

--- PI-Zadaća1/Form1.Designer.cs
+++ PI-Zadaća1/Form1.Designer.cs
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.imeprezime = new System.Windows.Forms.TextBox();
-            this.fakultet = new System.Windows.Forms.TextBox();
-            this.godina = new System.Windows.Forms.TextBox();
-            this.grad = new System.Windows.Forms.TextBox();
-            this.uloga = new System.Windows.Forms.TextBox();
-            this.ovisi = new System.Windows.Forms.TextBox();
-            this.datoteka = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -43,114 +43,110 @@
             this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // imeprezime
+            // textBox1
             // 
-            this.imeprezime.Location = new System.Drawing.Point(116, 97);
-            this.imeprezime.Name = "imeprezime";
-            this.imeprezime.Size = new System.Drawing.Size(100, 20);
-            this.imeprezime.TabIndex = 0;
+            this.textBox1.Location = new System.Drawing.Point(152, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
             // 
-            // fakultet
+            // textBox2
             // 
-            this.fakultet.Location = new System.Drawing.Point(116, 204);
-            this.fakultet.Name = "fakultet";
-            this.fakultet.Size = new System.Drawing.Size(100, 20);
-            this.fakultet.TabIndex = 1;
-            this.fakultet.TextChanged += new System.EventHandler(this.prezime_TextChanged);
+            this.textBox2.Location = new System.Drawing.Point(152, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
             // 
-            // godina
+            // textBox3
             // 
-            this.godina.Location = new System.Drawing.Point(116, 132);
-            this.godina.Name = "godina";
-            this.godina.Size = new System.Drawing.Size(100, 20);
-            this.godina.TabIndex = 2;
+            this.textBox3.Location = new System.Drawing.Point(152, 148);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
             // 
-            // grad
+            // textBox4
             // 
-            this.grad.Location = new System.Drawing.Point(116, 168);
-            this.grad.Name = "grad";
-            this.grad.Size = new System.Drawing.Size(100, 20);
-            this.grad.TabIndex = 3;
+            this.textBox4.Location = new System.Drawing.Point(152, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
             // 
-            // uloga
+            // textBox5
             // 
-            this.uloga.Location = new System.Drawing.Point(116, 240);
-            this.uloga.Name = "uloga";
-            this.uloga.Size = new System.Drawing.Size(100, 20);
-            this.uloga.TabIndex = 4;
-            this.uloga.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox5.Location = new System.Drawing.Point(152, 228);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
             // 
-            // ovisi
+            // textBox6
             // 
-            this.ovisi.Location = new System.Drawing.Point(116, 271);
-            this.ovisi.Name = "ovisi";
-            this.ovisi.Size = new System.Drawing.Size(100, 20);
-            this.ovisi.TabIndex = 5;
+            this.textBox6.Location = new System.Drawing.Point(152, 269);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
             // 
-            // datoteka
+            // button1
             // 
-            this.datoteka.Location = new System.Drawing.Point(12, 30);
-            this.datoteka.Name = "datoteka";
-            this.datoteka.Size = new System.Drawing.Size(104, 23);
-            this.datoteka.TabIndex = 6;
-            this.datoteka.Text = "Odaberi datoteku";
-            this.datoteka.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(30, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 100);
+            this.label1.Location = new System.Drawing.Point(47, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "Ime i prezime";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 135);
+            this.label2.Location = new System.Drawing.Point(47, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Godina rođenja";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 171);
+            this.label3.Location = new System.Drawing.Point(47, 151);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Mjesto rođenja";
+            this.label3.Text = "label3";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(65, 207);
+            this.label4.Location = new System.Drawing.Point(47, 187);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Fakultet";
+            this.label4.Text = "label4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(75, 243);
+            this.label5.Location = new System.Drawing.Point(47, 231);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 13);
             this.label5.TabIndex = 11;
-            this.label5.Text = "Uloga";
+            this.label5.Text = "label5";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(41, 274);
+            this.label6.Location = new System.Drawing.Point(47, 272);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.Size = new System.Drawing.Size(35, 13);
             this.label6.TabIndex = 12;
-            this.label6.Text = "*ovisi o ulozi*";
+            this.label6.Text = "label6";
             // 
             // Form1
             // 
@@ -163,13 +159,13 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.datoteka);
-            this.Controls.Add(this.ovisi);
-            this.Controls.Add(this.uloga);
-            this.Controls.Add(this.grad);
-            this.Controls.Add(this.godina);
-            this.Controls.Add(this.fakultet);
-            this.Controls.Add(this.imeprezime);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -180,13 +176,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox imeprezime;
-        private System.Windows.Forms.TextBox fakultet;
-        private System.Windows.Forms.TextBox godina;
-        private System.Windows.Forms.TextBox grad;
-        private System.Windows.Forms.TextBox uloga;
-        private System.Windows.Forms.TextBox ovisi;
-        private System.Windows.Forms.Button datoteka;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
--- PI-Zadaća1/Form1.cs
+++ PI-Zadaća1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,36 +11,21 @@ using System.Windows.Forms;
 
 namespace PI_Zadaća1
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+       public partial class Form1 : Form
         {
-            InitializeComponent();
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
         }
-
-        private void prezime_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
 }
+
+
+    
+
+    
